ab833943710aea54c3c766b18c3ff3fe0ac224bb
Thuan Nguyen <thuanday2222@gmail.com>
2024-06-17 14:04:25 +0700

    update

--- resources/views/frontend/partials/menu.blade.php
+++ resources/views/frontend/partials/menu.blade.php
@@ -1,13 +1,239 @@
 <div class="container wrapper-menu">
     <ul class="list-menu">
-        <li class="item-menu"><a href="#">Thực phẩm chức năng<i class="fa-solid fa-chevron-down ms-2"></i></a></li>
-        <li class="item-menu"><a href="#">Dược phẩm mỹ<i class="fa-solid fa-chevron-down ms-2"></i></a></li>
-        <li class="item-menu"><a href="#">Chăm sóc cá nhân<i class="fa-solid fa-chevron-down ms-2"></i></a></li>
-        <li class="item-menu"><a href="#">Thuốc<i class="fa-solid fa-chevron-down ms-2"></i></a></li>
-        <li class="item-menu"><a href="#">Thiết bị y tế<i class="fa-solid fa-chevron-down ms-2"></i></a></li>
-        <li class="item-menu"><a href="#">Tiêm chủng</a></li>
-        <li class="item-menu"><a href="#">Bệnh</a></li>
-        <li class="item-menu"><a href="#">Góc sức khỏe</a></li>
-        <li class="item-menu"><a href="#">Hệ thống nhà thuốc</a></li>
+        <li class="item-menu dropdown ">
+            {{-- <a href="#">Thực phẩm chức năng<i class="fa-solid fa-chevron-down ms-2"></i></a> --}}
+            <a data-bs-toggle="dropdown">
+                Thực phẩm chức năng
+                <i class="fa-solid fa-chevron-down ms-2"></i>
+            </a>
+            <ul class="dropdown-menu wrapper-menu" aria-labelledby="thucpham">
+                <div class="tab">
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-1')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Vitamin & Khoáng chất</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-2')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Sinh lý - Nội tiết tố</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-3')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Cải thiện tăng cường chức
+                        năng</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-4')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Hỗ trợ điều trị</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-5')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Hỗ trợ tiêu hóa
+                        năng</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-6')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Thần kinh não
+                        năng</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-7')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Hỗ trợ làm đẹp
+                        năng</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-8')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Sức khỏe tim mạch
+                        năng</button>
+                    <button class="tablinks" onmouseover="openTab(event, 'tab-9')">
+                        <i class="fa-solid fa-heart-pulse me-3" style="color: #0843ba;"></i>
+                        Dinh dưỡng</button>
+                </div>
+
+                <div id="tab-1" class="tabcontent">
+                    <div class="row section-product">
+                        <div class="col-lg-4 mb-4">
+                            <div class="wrapper-product_tab">
+                                <div class="product-img_tab">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/256x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/00021988_anica_phytextra_60v_5137_6347_large_81a2361ecd.jpg"
+                                        alt="Img-Product">
+                                </div>
+                                <p class="ms-3">Bổ sung Canxi & Vitamin D</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4">
+                            <div class="wrapper-product_tab">
+                                <div class="product-img_tab">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/256x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/00021988_anica_phytextra_60v_5137_6347_large_81a2361ecd.jpg"
+                                        alt="Img-Product">
+                                </div>
+                                <p class="ms-3">Bổ sung Canxi & Vitamin D</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4">
+                            <div class="wrapper-product_tab">
+                                <div class="product-img_tab">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/256x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/00021988_anica_phytextra_60v_5137_6347_large_81a2361ecd.jpg"
+                                        alt="Img-Product">
+                                </div>
+                                <p class="ms-3">Bổ sung Canxi & Vitamin D</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4">
+                            <div class="wrapper-product_tab">
+                                <div class="product-img_tab">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/256x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/00021988_anica_phytextra_60v_5137_6347_large_81a2361ecd.jpg"
+                                        alt="Img-Product">
+                                </div>
+                                <p class="ms-3">Bổ sung Canxi & Vitamin D</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4">
+                            <div class="wrapper-product_tab">
+                                <div class="product-img_tab">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/256x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/00021988_anica_phytextra_60v_5137_6347_large_81a2361ecd.jpg"
+                                        alt="Img-Product">
+                                </div>
+                                <p class="ms-3">Bổ sung Canxi & Vitamin D</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="section-product-selling">
+                        <div class="title-selling mb-3">
+                            <span class="me-3">
+                                Bán chạy nhất
+                            </span>
+                            <a href="#" class="link-product">
+                                <span>| Xem tất cả</span>
+                                <i class="fa-solid fa-angle-left ms-3" style="color: #2b5bc9"></i>
+                            </a>
+                        </div>
+
+                        <div class="wrapper-list-product-selling">
+                            <div class="box-product">
+                                <div class="product-selling__img">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/375x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/DSC_09006_a1b4671cf5.jpg"
+                                        alt="Hỉnh ảnh sản phẩm bán chạy">
+                                </div>
+                                <div class="product-selling__content">
+                                    <p>
+                                        Viên nang mềm NatureCare Omega 369 bổ sung thực phẩm chức năng
+                                    </p>
+                                </div>
+
+                                <div class="product-selling__price">
+                                    <p class="price-new"></p>
+                                    <p class="price-old"></p>
+                                </div>
+                            </div>
+                            <div class="box-product">
+                                <div class="product-selling__img">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/375x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/DSC_09006_a1b4671cf5.jpg"
+                                        alt="Hỉnh ảnh sản phẩm bán chạy">
+                                </div>
+                                <div class="product-selling__content">
+                                    <p>
+                                        Viên nang mềm NatureCare Omega 369 bổ sung thực phẩm chức năng
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="box-product">
+                                <div class="product-selling__img">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/375x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/DSC_09006_a1b4671cf5.jpg"
+                                        alt="Hỉnh ảnh sản phẩm bán chạy">
+                                </div>
+                                <div class="product-selling__content">
+                                    <p>
+                                        Viên nang mềm NatureCare Omega 369 bổ sung thực phẩm chức năng
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="box-product">
+                                <div class="product-selling__img">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/375x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/DSC_09006_a1b4671cf5.jpg"
+                                        alt="Hỉnh ảnh sản phẩm bán chạy">
+                                </div>
+                                <div class="product-selling__content">
+                                    <p>
+                                        Viên nang mềm NatureCare Omega 369 bổ sung thực phẩm chức năng
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="box-product">
+                                <div class="product-selling__img">
+                                    <img src="https://cdn.nhathuoclongchau.com.vn/unsafe/375x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/DSC_09006_a1b4671cf5.jpg"
+                                        alt="Hỉnh ảnh sản phẩm bán chạy">
+                                </div>
+                                <div class="product-selling__content">
+                                    <p>
+                                        Viên nang mềm NatureCare Omega 369 bổ sung thực phẩm chức năng
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="tab-2" class="tabcontent">
+                    tab 2
+                </div>
+                <div id="tab-3" class="tabcontent">
+                    tab 3
+                </div>
+                <div id="tab-4" class="tabcontent">
+                    tab 4
+                </div>
+                <div id="tab-5" class="tabcontent">
+                    tab 5
+                </div>
+                <div id="tab-6" class="tabcontent">
+                    tab 6
+                </div>
+                <div id="tab-7" class="tabcontent">
+                    tab 7
+                </div>
+                <div id="tab-8" class="tabcontent">
+                    tab 8
+                </div>
+                <div id="tab-9" class="tabcontent">
+                    tab 9
+                </div>
+                <div class="clearfix"></div>
+            </ul>
+        </li>
+        <li class="item-menu">
+            <a href="#">Dược phẩm mỹ<i class="fa-solid fa-chevron-down ms-2"></i></a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Chăm sóc cá nhân<i class="fa-solid fa-chevron-down ms-2"></i></a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Thuốc<i class="fa-solid fa-chevron-down ms-2"></i></a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Thiết bị y tế<i class="fa-solid fa-chevron-down ms-2"></i></a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Tiêm chủng</a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Bệnh</a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Góc sức khỏe</a>
+        </li>
+        <li class="item-menu">
+            <a href="#">Hệ thống nhà thuốc</a>
+        </li>
     </ul>
 </div>
+
+
+<script>
+    function openTab(evt, tabId) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabId).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+</script>
